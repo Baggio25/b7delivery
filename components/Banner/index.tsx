b14ca8fe11c9914fb0e200c5fa19a21a22db62b5
Rodrigo Baggio <rodrigo.baggio.si@gmail.com>
2022-11-07 10:19:07 -0300

    Aula 30

--- components/Banner/index.tsx
+++ components/Banner/index.tsx
@@ -1,16 +1,19 @@
 import { Swiper, SwiperSlide } from "swiper/react";
 
 import "swiper/css";
+
 import styles from "./Banner.module.css";
 
 export const Banner = () => {
   return (
     <div className={styles.container}>
-      <Swiper
-        slidesPerView={1}
-      >
-        <SwiperSlide>Slide 1</SwiperSlide>
-        <SwiperSlide>Slide 2</SwiperSlide>
+      <Swiper slidesPerView={1} className={styles.swiper}>
+        <SwiperSlide className={styles.slide}>
+          <div className={styles.slideImg}>1</div>
+        </SwiperSlide>
+        <SwiperSlide className={styles.slide}>
+          <div className={styles.slideImg}>2</div>
+        </SwiperSlide>
       </Swiper>
     </div>
   );
